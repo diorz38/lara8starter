0d3032919b2aeffb05ae7acfa4caf9a3a949adb4
diorz38 <diorz38@yahoo.co.id>
2020-11-13 14:21:13 +0700

    Add Main Module
    route: main

---
+++ Modules/Main/Resources/views/partials/_footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer footer-transparent d-print-none">
+    <div class="container">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ml-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item"><a href="./docs/index.html"
+                            class="link-secondary">Documentation</a></li>
+                    <li class="list-inline-item"><a href="./faq.html" class="link-secondary">FAQ</a></li>
+                    <li class="list-inline-item"><a href="https://github.com/tabler/tabler" target="_blank"
+                            class="link-secondary">Source code</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright © 2020
+                        <a href="." class="link-secondary">Tabler</a>.
+                        All rights reserved.
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="./changelog.html" class="link-secondary">v1.0.0-alpha.12</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/home', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('home');
+
 require __DIR__.'/auth.php';
 
 // Route::group([
